c01f6f6f43039ab15f7fe1d25b5f79364e2d4361
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-03-06 15:53:47 -0300

    release v0.0.2

--- core/server.py
+++ core/server.py
@@ -9,7 +9,7 @@ class Server:
         self.app.config.from_pyfile('config.py')
         self.api = Api(
             self.app,
-            version='0.0.2-SNAPSHOT',
+            version='0.0.2',
             title='JN Invest Bot',
             description='Api de web scraping e cálculos para mercado financeiro',
             doc='/doc'
